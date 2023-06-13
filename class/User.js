799935c164abc37d8ceae31d6872a5752ada5b53
Gabriel <70722542+GabrielPlazaola@users.noreply.github.com>
2023-06-13 07:03:23 -0300

    .

---
+++ class/User.js
@@ -0,0 +1,10 @@
+//creame una clase usuario que los atributos sean username, email y password
+
+class User {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+}
+module.exports = User;
